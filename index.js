f289035241862a71f1dde5d00b93f8cf1d273dee
Sara Palacio <saritapz01@hotmail.com>
2023-07-23 01:44:03 -0500

    Finishing the game

--- index.js
+++ index.js
@@ -1,40 +1,105 @@
-let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
 let userClickedPattern = [];
 
 //Levels
 let started = false;
 let level = 0;
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*4)
-    //console.log(randomNumber)
+//Start the game
+$(document).on( "keydown", function() {
+    alert( "Handler for `keydown` called." );
+    if (!started) {
+        $("#level-title").text("level " + level);
+        $('p').remove();
+        $('h1').text("Level 0");
+        nextSequence();
+        started = true;
+    }
+});
 
-    var randomChosenColour= buttonColours[randomNumber]
-    console.log('el color random es: ',randomChosenColour)
-
-    gamePattern.push(randomChosenColour)
-    console.log(gamePattern);
-
-    $('#' + randomChosenColour).delay(50).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playSound(randomChosenColour);
-
-
-    return;
-}
-
-//Check Which Button is Pressed//
+//Check Which Button is Pressed
 $('.btn').on('click', function(){
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
-    console.log('el color del usuario es:',userChosenColour);
-    console.log(userClickedPattern)
+    //console.log('el color presionado del usuario es:',userChosenColour);
+    //console.log('el patron del usuario fue: ',userClickedPattern)
     animatePress(userChosenColour) //Revisar acá
-    playSound(userChosenColour)
+    playSound(userChosenColour);
 
+    checkAnswer(userClickedPattern);
     return
 } )
+
+//Check the answers
+// function checkAnswer() {
+//     if(userClickedPattern.length > 0){
+//         if(userClickedPattern.length === gamePattern.length && JSON.stringify(userClickedPattern) === JSON.stringify(gamePattern)){
+//             alert('sucessssss :) ')
+//             setTimeout( function(){
+//                 nextSequence()
+//             }, 1000)
+//             userClickedPattern = [];
+//         }
+//         else{
+//             alert('wrong! :( ')
+//         }
+//     }
+// }
+
+function checkAnswer() {
+    if(userClickedPattern.length > 0){
+        if(userClickedPattern.length === gamePattern.length){
+            if(JSON.stringify(userClickedPattern) === JSON.stringify(gamePattern)){
+                console.log("Success ✨✔️ !");
+                //alert('sucessssss :) ')
+                setTimeout( function(){
+                    nextSequence()
+                }, 1000)
+            }
+            else{
+                console.log('wrong! :( ')
+                playSound('wrong');
+
+                $("#level-title").text("Game over. Press Any Key To Restart");
+                $('body').addClass("game-over");
+                setTimeout(function() {
+                    $('body').removeClass("game-over");
+                }, 200);
+
+                startOver()
+            }
+        }
+    }
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random()*4)
+    var randomChosenColour= buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    console.log('el color random del juego es: ',randomChosenColour)
+    console.log('patron del juego:',gamePattern);
+
+    $('#' + randomChosenColour).delay(50).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    checkAnswer(gamePattern);
+    return;
+}
+
+//Restart the game
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+    started = false;
+}
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
@@ -48,24 +113,4 @@ function animatePress(currentColor) {
     }, 100);
 }
 
-playSound()
-animatePress()
-
 //setInterval(nextSequence, 1500);
-
-//Start the game
-
-
-$(document).on( "keydown", function() {
-    alert( "Handler for `keydown` called." );
-
-    if (!started) {
-        $("#level-title").text("level " + level);
-        $('p').remove();
-        $('h1').text("Level 0");
-        nextSequence();
-        started = true 
-    }
-    nextSequence();
-
-} );
